b15b473f0cb21c94db68b0e4800632b8ea0fdffa
huchu501 <huchu501@gmail.com>
2022-09-27 15:52:27 -0700

    Eric | ComplexPlane.cpp (setCenter, setMouseLocation, loadText, countIterations)

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -4,4 +4,29 @@
 using namespace sf;
 using namespace std;
 
+// The center coordinate of the view is set by the given coordinate
+void ComplexPlane::setCenter(Vector2f coord)
+{
+	m_view.setCenter(Vector2f(coord));
+	m_view.setSize(Vector2f(1980.f, 1080.f));
+}
 
+// Update the mouse cursor position with the given coordinate value
+void ComplexPlane::setMouseLocation(Vector2f coord)
+{
+	m_mouseLocation = coord;
+}
+
+// Loads the information text (center, cursor, instructions) to the screen
+void ComplexPlane::loadText(Text& text)
+{
+	text.setString("Mandelbrot Set\nCenter: (");
+	// need to add more work
+}
+
+// Counting the # of iterations of the Mandelbrot set for the given coordinate
+size_t ComplexPlane::countIterations(Vector2f coord)
+{
+	int iterations = 0;
+	// need to add more work
+}
--- code/ComplexPlane.h
+++ code/ComplexPlane.h
@@ -7,6 +7,11 @@
 using namespace sf;
 using namespace std;
 
+const unsigned int MAX_ITER = 64;
+const float BASE_WIDTH = 4.0;
+const float BASE_HEIGHT = 4.0;
+const float BASE_ZOOM = 0.5;
+
 class ComplexPlane
 {
 	private:
